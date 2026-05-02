9985ba81b68df2e97d0a61a7c8c05b4346fb48e3
anokyebright4-dev <anokyebright4@gmail.com>
2026-05-03 00:13:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1006,7 +1006,7 @@ async def debug_upload(request: Request):
 
 if __name__ == "__main__":
   import os
-  port = int(os.environ.get(''PORT'', 8000))
+  port = int(os.environ.get('PORT', 8000))
     uvicorn.run(
         app,
         host="0.0.0.0",
