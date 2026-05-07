05c81e760fd957adbc50a162d1ccb85fca113c11
anokyebright4-dev <anokyebright4@gmail.com>
2026-05-08 00:15:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -909,14 +909,22 @@ async def register_unit(
 
     package_hash = hashlib.sha256(package_bytes).hexdigest()
     seal_hash = hashlib.sha256(seal_bytes).hexdigest()
+       package_file_path = f"uploads/{package_hash}.jpg"
+    seal_file_path = f"uploads/{seal_hash}.jpg"
+
+    with open(package_file_path, "wb") as f:
+        f.write(package_bytes)
+
+    with open(seal_file_path, "wb") as f:
+        f.write(seal_bytes) 
 
     unit_id = create_unit_record(
         product_name,
         brand,
         batch_code,
-        package_image.filename,
+        package_file_path,
         package_hash,
-        seal_image.filename,
+        seal_file_path,
         seal_hash
     )
 
