6c10b0e473c85a5acdfa79cf677cfb65ae1fa00c
anokyebright4-dev <anokyebright4@gmail.com>
2026-05-20 21:12:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1238,8 +1238,8 @@ if package_image is None and seal_image is None:
         )
     else:
         unit_id = product_id
-
-        return {
+        
+    return {
         "status": "raw_registered",
         "unit_id": unit_id,
         "product_id": product_id,
