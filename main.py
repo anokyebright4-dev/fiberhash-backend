a4ffaa614ce74b927867add2bcd5643e75ddf918
anokyebright4-dev <anokyebright4@gmail.com>
2026-06-07 22:38:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1902,7 +1902,7 @@ if seller_row:
         conn.close()
 
     if updated_count == 0:
-         return {
+        return {
                  "status": "error",
                  "message": "Challenge request not found.",
          }
